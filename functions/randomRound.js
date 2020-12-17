bfea25410ffab10f37f46a5c8d9aa0a9d9db559a
Žiga Lah <ziga.lah999@gmail.com>
2020-12-17 21:29:37 +0100

    Added eslint

--- functions/randomRound.js
+++ functions/randomRound.js
@@ -1,18 +1,19 @@
 module.exports = {
-	name: 'runda',
-	description: 'Kdo bo dal za rundo?',
-	execute(message, args) {
-        const server = message.guild;
-        const members = server.members.cache;
-        const selection = members.filter((el) => !el.user.bot);
-        const selectedNumber = Math.floor(Math.random() * Math.floor(selection.size));
+  name: 'runda',
+  description: 'Kdo bo dal za rundo?',
+  execute(message, args) {
+    const server = message.guild;
+    const members = server.members.cache;
+    const selection = members.filter((el) => !el.user.bot);
+    const selectedNr = Math.floor(Math.random() * Math.floor(selection.size));
 
-        let i = 0;
-        selection.forEach((member) =>{
-            if(selectedNumber == i++){
-                args.channels.cache.get('789210047154356254').send(`<@${member.user.id}> bo za rundo dal!`);
-            }
-        })
-        message.delete();
-	},
+    let i = 0;
+    selection.forEach((member) =>{
+      if (selectedNr == i++) {
+        args.channels.cache.get('789210047154356254').
+            send(`<@${member.user.id}> bo za rundo dal!`);
+      }
+    });
+    message.delete();
+  },
 };
--- index.js
+++ index.js
@@ -1,39 +1,42 @@
 const Discord = require('discord.js');
 const greet = require('./functions/greetings').greet;
+const secretKey = require('./secrets').secretKey;
+
+// Import for commands
 const ping = require('./functions/getInterventionsData');
 const round = require('./functions/randomRound');
-const secretKey = require('./secrets').secretKey;
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
 client.on('ready', ()=>{
-    console.log('I\'m ready');
+  console.log('I\'m ready');
 });
 
+client.on('guildMemberAdd', (member) => greet(member));
+
+// set commands
 client.commands.set(ping.name, ping);
 client.commands.set(round.name, round);
 
-client.on('guildMemberAdd', member => greet(member))
+client.on('message', (msg) =>{
+  if (!msg.content.startsWith('.') || msg.author.bot) return;
 
-client.on('message', msg =>{
-    if (!msg.content.startsWith('.') || msg.author.bot) return;
+  let args = msg.content.slice('.'.length).trim().split(/ +/); // get arguments
+  const command = args.shift().toLowerCase();
 
-    let args = msg.content.slice('.'.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-    if (!client.commands.has(command)) return;
+  if (!client.commands.has(command)) return;
 
-    try {
-        if(command == 'runda'){
-            args = client;
-        }
-        client.commands.get(command).execute(msg, args);
-    } catch (error) {
-        console.error(error);
-        msg.reply('there was an error trying to execute that command!');
+  try {
+    if (command == 'runda') {
+      args = client;
     }
-    
-})
+    client.commands.get(command).execute(msg, args);
+  } catch (error) {
+    console.log(error);
+    msg.reply('alo alo, tu neke nea stima!');
+  }
+});
 
 
 client.login(secretKey);
